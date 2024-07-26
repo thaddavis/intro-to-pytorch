cc486108db762e3c97864e00c2770349c7a22de0
tad dy <a@a.com>
2024-07-25 22:31:24 -0400

    fix typo

--- plt_one_addpt_onclick.py
+++ plt_one_addpt_onclick.py
@@ -22,7 +22,7 @@ class plt_one_addpt_onclick:
         fig.canvas.footer_visible = False
 
         plt.subplots_adjust(bottom=0.25)
-        ax.scatter(x[pos], y[pos], marker='x', s=80, c = 'cool', label="malignant")
+        ax.scatter(x[pos], y[pos], marker='x', s=80, c = 'red', label="cool")
         ax.scatter(x[neg], y[neg], marker='o', s=100, label="wack", facecolors='none', edgecolors=dlblue,lw=3)
         ax.set_ylim(-0.05,1.1)
         xlim = ax.get_xlim()
