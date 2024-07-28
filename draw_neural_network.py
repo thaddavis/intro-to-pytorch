837f773a9edb06d43fc8e8ebe2290779a9fc883b
tad dy <a@a.com>
2024-07-27 21:48:28 -0400

    testing

--- draw_neural_network.py
+++ draw_neural_network.py
@@ -26,13 +26,13 @@ def draw_neural_net(ax, left, right, bottom, top, layer_sizes):
             # if layer_size > 1: 
             if n == 0:
                 ax.annotate(f'Input {m+1}', (n*h_spacing + left, layer_top - m*v_spacing), 
-                            textcoords="offset points", xytext=(-25,25*v_spacing), ha='center', fontsize=12, color='blue')
+                            textcoords="offset points", xytext=(-25,25 + v_spacing), ha='center', fontsize=12, color='blue')
             elif n == len(layer_sizes) - 1:
                 ax.annotate(f'Output {m+1}', (n*h_spacing + left, layer_top - m*v_spacing), 
-                            textcoords="offset points", xytext=(25,25*v_spacing), ha='center', fontsize=12, color='blue')
+                            textcoords="offset points", xytext=(25,25 + v_spacing), ha='center', fontsize=12, color='blue')
             else:
                 ax.annotate(f'Layer {n}-{m+1}', (n*h_spacing + left, layer_top - m*v_spacing), 
-                            textcoords="offset points", xytext=(0,25*v_spacing), ha='center', fontsize=12, color='blue')
+                            textcoords="offset points", xytext=(0,25 + v_spacing), ha='center', fontsize=12, color='blue')
     
     # Edges
     for n, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
